1d9ad8b22ce86a24e3c1167be00f2e79d4897f6d
KartikChugh <kartikchugh.kc@gmail.com>
2020-08-19 22:42:51 -0400

    Create game with Mario, coins, & hazards
    - Use right/left arrow keys + space to control Mario
    
    - Gain points for surviving and collecting coins
    
    - Dodge spikeball hazards, or lose a life
    
    - Lose three lives until Game Over, where you can view your score

---
+++ code.js
@@ -0,0 +1,445 @@
+var MS_PER_TICK = 10;
+
+var KEY = {SPACE:32, RIGHT:39, LEFT:37};
+var CSTATE = {FREE:0, RIGID:1};
+var VSTATE = {REST:0, JUMP:1};
+var HSTATE = {REST_RIGHT:0, REST_LEFT:1, WALK_RIGHT:2, WALK_LEFT:3};
+
+var COIN_IDS = [0,1,2,3,4];
+var HAZARD_IDS = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+
+var mario;
+var coins, hazards, coins_despawn, hazards_despawn;
+var ticks, score, lives;
+var highScore = 0;
+
+var curtain;
+var scoring, highScoring;
+
+// UTILITY
+
+function isColliding(elem, width, height) {
+  var rect1 = {x:mario.x, y:mario.y, width:40, height:36};
+  var rect2 = {x:getXPosition(elem), y:getYPosition(elem), width:width, height:height};
+  
+  if (rect1.x < rect2.x + rect2.width &&
+   rect1.x + rect1.width > rect2.x &&
+   rect1.y < rect2.y + rect2.height &&
+   rect1.y + rect1.height > rect2.y) {
+    return true;
+  }
+  return false;
+}
+
+function formatScore(scoreNum) {
+  return ("0000000" + ~~scoreNum).slice(-7);
+}
+
+function updateScore(delta) {
+  score += delta;
+  setText("score", formatScore(score));
+}
+
+function updateLives(delta) {
+  lives += delta;
+  setText("lives", ("⨯ " + lives));
+  if (lives === 0) endGame();
+  else mario.invincibility = 10;
+}
+
+// HAZARD
+
+function updateHazardY(h) {
+  h.y += h.vy;
+  setPosition(h.getStr(),h.x,h.y);
+}
+
+function scheduleHazardDespawn(i, h) {
+  hazards_despawn.push(i);
+  hideElement(h.getStr());
+}
+
+function performHazardsDespawn() {
+  var offset = 0;
+  for (var i in hazards_despawn) {
+    var hazardIndex = hazards_despawn[i] - offset;
+    hazards.splice(hazardIndex, 1);
+    offset++;
+  }
+}
+
+function updateHazardStatus(i, h) {
+  if (h.y >= 440) scheduleHazardDespawn(i, h);
+  else if (isColliding(h.getStr(), 32, 32) && mario.invincibility === -1) {
+    updateLives(-1);
+    scheduleHazardDespawn(i, h);
+  }
+}
+
+function updateHazards() {
+  hazards_despawn = [];
+  for (var i in hazards) {
+    var h = hazards[i];
+    updateHazardY(h);
+    updateHazardStatus(i, h);
+  }
+  performHazardsDespawn();  
+}
+
+function spawnHazard() {
+  var spawnH = new Hazard(randomNumber(0,290),-30);
+  var spawnId = 0;
+  for (var i in HAZARD_IDS) {
+    var HAZARD_ID = HAZARD_IDS[i];
+    var present = false;
+    for (var j in hazards) {
+      var hazard = hazards[j];
+      present = present || (hazard.id===HAZARD_ID);
+    }
+    if (present === false) {
+      spawnId = HAZARD_ID;
+      break;
+    }
+  }
+  
+  spawnH.id = spawnId;
+  hazards.push(spawnH);
+  showElement(spawnH.getStr());
+}
+
+function Hazard(x, y) {
+  this.x = x;
+  this.y = y;
+  this.vy = 3;
+  this.getStr = function() {
+    return "hazard" + this.id;
+  };
+}
+
+// COIN
+
+function updateCoinSprite(c) {
+  c.frame = (c.frame+1)%8;
+  setImageURL(c.getStr(), "assets/coin"+c.frame+".gif");
+}
+
+function updateCoinY(c) {
+  c.y += c.vy;
+  setPosition(c.getStr(),c.x,c.y);
+}
+
+function scheduleCoinDespawn(i, c) {
+  coins_despawn.push(i);
+  hideElement(c.getStr());
+}
+
+function performCoinsDespawn() {
+  for (var i in coins_despawn) {
+    var coinIndex = coins_despawn[i];
+    coins.splice(coinIndex, 1);
+  }
+}
+
+function updateCoinStatus(i, c) {
+  if (c.y >= 440) scheduleCoinDespawn(i, c);
+    
+  else if (isColliding(c.getStr(), 32, 32)) {
+    playSound("assets/coin.mp3", false);
+    updateScore(200);
+    scheduleCoinDespawn(i, c);
+  }
+}
+
+function updateCoins() {
+  coins_despawn = [];
+  for (var i in coins) {
+    var c = coins[i];
+    
+    updateCoinY(c);
+    updateCoinStatus(i, c);
+    
+    if (ticks % 7 === 0) updateCoinSprite(c);
+  }
+  
+  performCoinsDespawn();    
+}
+
+function spawnCoin() {
+  var spawnC = new Coin(randomNumber(0,290),-30);
+  var spawnId = 0;
+  for (var i in COIN_IDS) {
+    var COIN_ID = COIN_IDS[i];
+    var present = false;
+    for (var j in coins) {
+      var coin = coins[j];
+      present = present || (coin.id===COIN_ID);
+    }
+    if (present === false) {
+      spawnId = COIN_ID;
+      break;
+    }
+  }
+  spawnC.id = spawnId;
+  coins.push(spawnC);
+  showElement(spawnC.getStr());
+}
+
+function Coin(x, y) {
+  this.x = x;
+  this.y = y;
+  this.vy = 3;
+  this.frame = 0;
+  this.getStr = function() {
+    return "coin" + this.id;
+  };
+}
+
+// MARIO
+
+function updateMario() {
+  updateMarioY();
+  updateMarioX();
+  setPosition("mario",mario.x,mario.y);
+  if (ticks % 10 === 0) updateMarioVisibility();
+  if (ticks % 10 === 0) updateMarioSprite();
+}
+
+function updateMarioY() {
+  mario.vy += mario.ay;
+  mario.y += mario.vy;
+
+  if (mario.cstate === CSTATE.RIGID) {
+    if (mario.y >= 450 && curtain == 1) {
+      mario.ay = 0;
+      mario.vy = 0;
+      showElement("curtain");
+      narrowScreen();
+    }
+    return;
+  }
+  
+  if (mario.y >= 415-48) {
+    mario.vstate = VSTATE.REST;
+    mario.vy = 0;
+    mario.y = 415-48;
+  }
+}
+
+function updateMarioX() {
+  if (mario.cstate === CSTATE.RIGID) return;
+  if (mario.hstate === HSTATE.WALK_RIGHT) {
+    mario.x += 3;
+  } else if (mario.hstate === HSTATE.WALK_LEFT) {
+    mario.x -= 3;
+  }
+}
+
+function updateMarioVisibility() {
+  if (mario.invincibility === 0) {
+    showElement("mario"); 
+    mario.invincibility--;
+  }
+  if (mario.invincibility === -1) return;
+  if (mario.invincibility % 2 === 0) {
+    showElement("mario");
+  } else {
+    hideElement("mario");
+  }
+  mario.invincibility--;
+}
+
+function updateMarioSprite() {
+  
+  if (mario.cstate === CSTATE.RIGID) return;
+  
+  if (mario.hstate === HSTATE.REST_RIGHT) {
+    setImageURL("mario","assets/right.png");
+  } else if (mario.hstate === HSTATE.REST_LEFT) {
+    setImageURL("mario","assets/left.png");
+    
+  } else if (mario.hstate === HSTATE.WALK_RIGHT) {
+    if (getImageURL("mario") === "assets/rightB.png" && mario.vstate === VSTATE.REST) {
+      setImageURL("mario","assets/right.png");
+    } else {
+      setImageURL("mario","assets/rightB.png");
+    }
+  } else if (mario.hstate === HSTATE.WALK_LEFT) {
+    if (getImageURL("mario") === "assets/leftB.png"  && mario.vstate === VSTATE.REST) {
+      setImageURL("mario","assets/left.png");
+    } else {
+      setImageURL("mario","assets/leftB.png");
+    }
+  }
+}
+
+// MAIN ENGINE
+
+function initializeVars() {
+  mario = {
+    x: 25, y: 215,
+    vx: 0, vy: 0,
+    ay: 0.2,
+    vstate: VSTATE.REST,
+    hstate: HSTATE.REST_RIGHT,
+    cstate: CSTATE.FREE,
+    invincibility: -1,
+  };
+  coins = []; 
+  hazards = [];
+  coins_despawn = [];
+  hazards_despawn = [];
+  ticks = 0;
+  score = 0;
+  lives = 3;
+  setText("score", formatScore(score));
+  setText("lives", ("⨯ " + lives));
+  curtain = 1;
+  scoring = 0; 
+  highScoring = 0;
+}
+
+function cleanScreen() {
+  for (var i in COIN_IDS) {
+    var COIN_ID = COIN_IDS[i];
+    hideElement("coin" + COIN_ID);
+  }
+  for (var j in HAZARD_IDS) {
+    var HAZARD_ID = HAZARD_IDS[j];
+    hideElement("hazard" + HAZARD_ID);
+  }
+}
+
+function startGame() {
+  cleanScreen();
+  initializeVars();
+  playSound("assets/overworld.mp3", true);
+  setScreen("game");
+  tick();
+}
+
+function endGame() {
+  stopSound("assets/overworld.mp3");
+  highScore = Math.max(score, highScore);
+  setImageURL("mario","assets/deathA.png");
+  mario.cstate = CSTATE.RIGID;
+  mario.vy = 0;
+  mario.ay = 0;
+  setTimeout(function() {
+    mario.ay = 0.10;
+    mario.vy = -5;
+  }, MS_PER_TICK * 75);
+}
+
+function narrowScreen() {
+  if (curtain >= 450) {
+    showScoring();
+    return;
+  }
+  curtain += 5;
+  setSize("curtain", 320, curtain);
+  setTimeout(function() {narrowScreen()}, MS_PER_TICK);
+}
+
+function showScoring() {
+  setScreen("lose");
+  hideElement("curtain");
+  
+  setTimeout(function() {
+    hideElement("loseTitle");
+    setTimeout(function() {
+      setScreen("scoring");
+      incrementScoring();
+    }, MS_PER_TICK * 75);
+  }, MS_PER_TICK * 325);
+}
+
+function incrementScoring() {
+  if (scoring === score && highScoring === highScore) {
+    showElement("scoringSubtitle");
+    return;
+  }
+  var scoringDelta = score/50;
+  scoring = Math.min(scoring+scoringDelta, score);
+  setText("yourScore", formatScore(scoring));
+  
+  var highScoringDelta = highScore/50;
+  highScoring = Math.min(highScoring+highScoringDelta, highScore);
+  setText("highScore", formatScore(highScoring));
+  
+  setTimeout(function(){incrementScoring()}, MS_PER_TICK);
+}
+
+function tick() {
+  
+  setTimeout(function() {
+    ticks++;
+    tick();
+  }, MS_PER_TICK);
+  
+  updateMario();
+  
+  if (mario.cstate === CSTATE.RIGID) return;
+  
+  if (ticks % 40 === 0) spawnCoin();
+  updateCoins();
+  
+  if (ticks % (80 - 10 * ~~(ticks / 1000)) === 0) {
+    var r = Math.random();
+    if (r < 0.75) spawnHazard();
+    if (r < 0.1) spawnHazard();
+    if (r < 0.02) spawnHazard();
+  }
+  updateHazards();
+  
+  if (ticks % 50 === 0) updateScore(5);
+  
+}
+
+// KEY
+
+onEvent("game", "keydown", function(event) {
+  
+  if (mario.cstate === CSTATE.RIGID) return;
+  
+  if (event.keyCode === 13) endGame(); // remove
+  
+  // JUMP: only if at vertical rest
+  if (event.keyCode === KEY.SPACE && mario.vstate === VSTATE.REST) {
+    mario.vy = -7;
+    mario.vstate = VSTATE.JUMP;
+  }
+  
+  // WALK
+  if (event.keyCode === KEY.RIGHT) {
+    mario.hstate = HSTATE.WALK_RIGHT;
+  } else if (event.keyCode === KEY.LEFT) {
+    mario.hstate = HSTATE.WALK_LEFT;
+  } 
+
+});
+
+onEvent("game", "keyup", function(event){
+  
+  if (mario.cstate === CSTATE.RIGID) return;
+  
+  // STOP WALK
+  if (event.keyCode === KEY.RIGHT && mario.hstate === HSTATE.WALK_RIGHT) {
+    mario.hstate = HSTATE.REST_RIGHT;
+  } else if (event.keyCode === KEY.LEFT && mario.hstate === HSTATE.WALK_LEFT) {
+    mario.hstate = HSTATE.REST_LEFT;
+  }
+});
+
+onEvent("home", "keydown", function(event) {
+  if (event.keyCode === KEY.SPACE) {
+    startGame();
+  } 
+});
+
+onEvent("scoring", "keydown", function(event) {
+  if (event.keyCode === KEY.SPACE) {
+    setScreen("home");
+  } 
+});
+
